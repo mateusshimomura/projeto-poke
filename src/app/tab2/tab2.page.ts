1ef5ad25df5a9b5a5429cd49b86418be5dbc08a6
mateusshimomura <mateus_shimomura@hotmail.com>
2025-06-16 22:12:42 -0300

    adicionar/remover pokemons aos favoritos na tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -17,6 +17,7 @@ export class Tab2Page implements OnInit {
 
   nomePokemon: string | null = '';
   dadosPokemon: any;
+  favorito: boolean = false;
 
   ngOnInit() {
     this.nomePokemon = this.route.snapshot.paramMap.get('name');
@@ -26,8 +27,36 @@ export class Tab2Page implements OnInit {
         .subscribe((pokemon: any) => {
           this.dadosPokemon = pokemon;
           this.apiService.setPokemonSelecionado(pokemon);
+          if (this.getFavoritos().find((f: any) => f === pokemon.id)) {
+            this.favorito = true;
+          }
         });
     }
+  }
+
+  adiocionarFavorito() {
+    let favoritosId = JSON.parse(localStorage.getItem('favoritos') || '[]');
+    favoritosId.push(this.dadosPokemon.id);
+    localStorage.setItem('favoritos', JSON.stringify(favoritosId));
+  }
+
+  getFavoritos() {
+    return JSON.parse(localStorage.getItem('favoritos') || '[]');
+  }
+
+  removeFavorito() {
+    let favoritos = this.getFavoritos();
+    favoritos = favoritos.filter((f: any) => f !== this.dadosPokemon.id);
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+  }
+
+  mudarFavorito() {
+    if (this.favorito) {
+      this.removeFavorito();
+    } else {
+      this.adiocionarFavorito();
+    }
+    this.favorito = !this.favorito;
   }
 
   detalhesPokemon() {
