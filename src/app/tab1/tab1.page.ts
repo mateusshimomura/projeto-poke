446da51417809a12453bc837acfede0a2a65d294
mateusshimomura <mateus_shimomura@hotmail.com>
2025-06-16 23:00:25 -0300

    scroll infinito na lista de pokemons (tab1)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Router } from '@angular/router';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,15 +11,26 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page implements OnInit {
   constructor(private apiService: ApiService, private router: Router) {}
-  listaPokemons: any;
+  listaPokemons: any = [];
 
   ngOnInit() {
-    this.apiService.getLista(20, 0).subscribe((lista: any) => {
-      this.listaPokemons = lista.results;
-    });
+    this.getLista(20, 0);
   }
 
   escolherPokemon(nome: string) {
     this.router.navigate(['/tabs/tab2', nome]);
+  }
+
+  getLista(limit: number, offset: number, event?: InfiniteScrollCustomEvent) {
+    this.apiService.getLista(limit, offset).subscribe((lista: any) => {
+      this.listaPokemons = [...this.listaPokemons, ...lista.results];
+      if (event) {
+        event.target.complete();
+      }
+    });
+  }
+
+  onIonInfinite(event: InfiniteScrollCustomEvent) {
+    this.getLista(20, this.listaPokemons.length, event);
   }
 }
