74f17944adf3795570f5e1b8156fa8c48dfd2909
mateusshimomura <mateus_shimomura@hotmail.com>
2025-07-02 14:19:54 -0300

    imagem ao lado do nome de cada pokemon na lista

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../services/api.service';
 import { Router } from '@angular/router';
 import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import { StringUtilsService } from '../services/string-utils.service';
+import { forkJoin, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -17,6 +18,7 @@ export class Tab1Page implements OnInit {
     public stringUtils: StringUtilsService
   ) {}
   listaPokemons: any = [];
+  isLoading: boolean = false;
 
   ngOnInit() {
     this.getLista(20, 0);
@@ -27,15 +29,30 @@ export class Tab1Page implements OnInit {
   }
 
   getLista(limit: number, offset: number, event?: InfiniteScrollCustomEvent) {
-    this.apiService.getLista(limit, offset).subscribe((lista: any) => {
-      this.listaPokemons = [...this.listaPokemons, ...lista.results];
-      if (event) {
-        event.target.complete();
-      }
-    });
+    this.isLoading = true;
+    this.apiService
+      .getLista(limit, offset)
+      .pipe(
+        switchMap((response: any) => {
+          const lista = response.results.map((pokemon: any) =>
+            this.apiService.getDadosPokemon(pokemon.name).pipe(
+              map((p: any) => ({
+                name: p.name,
+                image: p.sprites.other.dream_world.front_default,
+              }))
+            )
+          );
+          return forkJoin(lista);
+        })
+      )
+      .subscribe((pokemons: any) => {
+        this.listaPokemons = [...this.listaPokemons, ...pokemons];
+        this.isLoading = false;
+        if (event) event.target.complete();
+      });
   }
 
   onIonInfinite(event: InfiniteScrollCustomEvent) {
-    this.getLista(20, this.listaPokemons.length, event);
+    if (!this.isLoading) this.getLista(20, this.listaPokemons.length, event);
   }
 }
